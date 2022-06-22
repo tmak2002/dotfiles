8dd555f598a429430c0e05c8bed6b633a91b9b1b
tmak2002 <tmak2002@mailbox.org>
2022-06-22 16:08:32 +0200

    update

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ cp -v .xinitrc /home/torben
 cp -v .gitconfig /home/torben
 cp -v .bashrc /home/torben
 mkdir -p /home/torben/.config
-cp -v -r .config /home/torben/.config
+cp -v -r .config /home/torben
+chown -R torben:torben /home/torben
